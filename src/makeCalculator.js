8ab3231178fe44facfe3f1f4294d34249cbe8878
Bohuslav Stanishevskyy <bohuslavstanishevskyy@MacBook-Pro-Bohuslav.local>
2026-07-10 14:00:00 +0300

    feat: complete calculator functionality

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,34 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+    operate(operation, number) {
+      this.result = operation(this.result, number);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    result: 0,
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
